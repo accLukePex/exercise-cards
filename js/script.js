3b7594547863999b7189e95abc4190458dd4d5a4
accLukePex <luca.pessione@accenture.com>
2024-06-06 19:10:48 +0200

    added cats images with API

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 /* 
---- GENERAZIONE CARD NEL DOM
+--- FUNZIONE PER GENERARE LE CARD
 */
 
 // Inserisco il div row all'interno di una variabile
@@ -19,10 +19,51 @@ for (let i = 0;  i < 10; i++) {
     // Aggiungiamo la classe col
     card.classList.add("card");
 
-
     // Aggiungere il contenuto al div
     col.append(card);
 
     // Aggiungere il div alla row
     row.append(col);
 }
+
+
+
+function populateCardsWithImages(cats) {
+    // seleziono le cards
+    const cards = document.querySelectorAll('.card');
+
+    cats.forEach((cat, index) => {
+        if (cards[index]) {
+            const img = document.createElement('img');
+            img.src = cat.url;
+
+            cards[index].innerHTML = '';
+
+            cards[index].append(img);
+        }
+    })
+}
+
+/*
+--- CHIAMATA API
+*/
+
+// URL dell'API
+const url = 'https://api.thecatapi.com/v1/images/search?limit=10';
+
+// Effettuare la chiamata GET usando fetch
+fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        populateCardsWithImages(data);
+    })
+    .catch(error => console.error('Error fetching the cat images:', error));
+
+
+
+
+/*
+--- LOAD MORE BUTTON
+*/
+
