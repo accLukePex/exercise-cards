567376e5c40ab916f03089f710ce3c362909e51f
accLukePex <luca.pessione@accenture.com>
2024-06-07 11:23:30 +0200

    added load-more button

--- js/script.js
+++ js/script.js
@@ -1,12 +1,8 @@
-/* 
---- FUNZIONE PER GENERARE LE CARD
-*/
-
 // Inserisco il div row all'interno di una variabile
 const row = document.querySelector(".row");
 
 // Generiamo i quadrati
-for (let i = 0;  i < 10; i++) {
+for (let i = 0;  i < 30; i++) {
     console.log(i);
 
     // Creiamo il div col generico
@@ -26,23 +22,32 @@ for (let i = 0;  i < 10; i++) {
     row.append(col);
 }
 
+// ----------------------------------------
 
 
+// FUNZIONE PER POPOLARE LE CARD CON LE IMMAGINI DEI GATTI
 function populateCardsWithImages(cats) {
     // seleziono le cards
     const cards = document.querySelectorAll('.card');
 
     cats.forEach((cat, index) => {
         if (cards[index]) {
+            // Creo l'elemento img e imposto l'attributo src all'URL dell'immagine
             const img = document.createElement('img');
             img.src = cat.url;
 
+            // Rimuovo i precedenti contenuti della card
             cards[index].innerHTML = '';
 
+            // Aggiungo l'immagine al div card
             cards[index].append(img);
         }
-    })
+    });
 }
+
+
+// --------------------------------------
+
 
 /*
 --- CHIAMATA API
@@ -51,7 +56,7 @@ function populateCardsWithImages(cats) {
 // URL dell'API
 const url = 'https://api.thecatapi.com/v1/images/search?limit=10';
 
-// Effettuare la chiamata GET usando fetch
+// Effettuo la chiamata GET usando fetch
 fetch(url)
     .then(response => response.json())
     .then(data => {
@@ -67,3 +72,21 @@ fetch(url)
 --- LOAD MORE BUTTON
 */
 
+let loadmore = document.querySelector('.load-more');
+let currentItem = 10;
+
+loadmore.addEventListener("click",
+
+    function() {
+        let columns = [...document.querySelectorAll('.col')];
+
+        for (let i = currentItem; i < currentItem + 10; i++) {
+            columns[i].style.display = 'inline-block';
+        }
+        currentItem += 10;
+
+        if(currentItem >= columns.length) {
+            loadmore.style.display = 'none';
+        }
+    }
+)
